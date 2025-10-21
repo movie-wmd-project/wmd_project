fa563bb92a42f253c171ed2013cbdd981acab79e
walid ghazi <walidkurdish01@gmail.com>
2025-10-21 19:07:07 +0300

    update from walid

--- index.js
+++ index.js
@@ -6,228 +6,326 @@ bg_title.src = image_bg[random]
 }, 5000);
 
 
+let open_menu = document.getElementById("open-menu")
+let menu_a = document.getElementById("menu-a")
+open_menu.addEventListener("click",function(){
+  menu_a.style.display="block"
+})
+
+let b_m_c = document.getElementById("b-m-c")
+b_m_c.addEventListener("click",function(){
+menu_a.style.display="none"
+console.log(1);
+  
+})
 
 
-const data_c1=[
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:5,
-    coalite:"4k",
-    type:"Action"
 
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:5,
-    coalite:"4k",
-    type:"Action"
-
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:5,
-    coalite:"4k",
-    type:"Action"
-
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:5,
-    coalite:"4k",
-    type:"Action"
-
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:5,
-    coalite:"4k",
-    type:"Action"
-
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:5,
-    coalite:"4k",
-    type:"Action"
-
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:5,
-    coalite:"4k",
-    type:"Action"
-
-},
-]
-
-
-const data_c2=[
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:2,
-    coalite:"4k",
-    type:"Action"
-
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:2,
-    coalite:"4k",
-    type:"Action"
-
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:2,
-    coalite:"4k",
-    type:"Action"
-
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:2,
-    coalite:"4k",
-    type:"Action"
-
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:2,
-    coalite:"4k",
-    type:"Action"
-
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:2,
-    coalite:"4k",
-    type:"Action"
-
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:2,
-    coalite:"4k",
-    type:"Action"
-
-},
-]
+const data_c1 = [
+  {
+    name: "One Battle After Another",
+    img: "one_battle_after_another.jpg",
+    time: "2h 41min",
+    yer: 2025,
+    reting: 5,
+    coalite: "4k",
+    type: "Action/Thriller"
+  },
+  {
+    name: "F1: The Movie",
+    img: "f1_the_movie2025.jpg",
+    time: "2h 35min",
+    yer: 2025,
+    reting: 4.5,
+    coalite: "4k",
+    type: "Sports/Action"
+  },
+  {
+    name: "Sinners",
+    img: "sinners2025.jpg",
+    time: "2h 17min",
+    yer: 2025,
+    reting: 5,
+    coalite: "4k",
+    type: "Horror/Drama"
+  },
+  {
+    name: "Diablo",
+    img: "diablo2025.jpg",
+    time: "1h 55min",
+    yer: 2025,
+    reting: 4.5,
+    coalite: "4k",
+    type: "Martial Arts/Action"
+  },
+  {
+    name: "Lilo & Stitch",
+    img: "lilo_stitch2025.jpg",
+    time: "2h 00min",
+    yer: 2025,
+    reting: 4.5,
+    coalite: "4k",
+    type: "Family/Adventure"
+  },
+  {
+    name: "Mission: Impossible – The Final Reckoning",
+    img: "mission_impossible_final_reckoning.jpg",
+    time: "2h 49min",
+    yer: 2025,
+    reting: 4.5,
+    coalite: "4k",
+    type: "Action/Spy"
+  },
+  {
+    name: "The Fantastic Four: First Steps",
+    img: "fantastic_four_first_steps.jpg",
+    time: "1h 54min",
+    yer: 2025,
+    reting: 4,
+    coalite: "4k",
+    type: "Superhero"
+  },
+  {
+    name: "How to Train Your Dragon (2025)",
+    img: "how_to_train_your_dragon2025.jpg",
+    time: "2h 04min",
+    yer: 2025,
+    reting: 4,
+    coalite: "4k",
+    type: "Adventure"
+  },
+  {
+    name: "Oppenheimer",
+    img: "Oppenheimer.jpg",
+    time: "1h 55min",
+    yer: 2025,
+    reting: 3.5,
+    coalite: "4k",
+    type: "Horror/Thriller"
+  },
+  
+  {
+    name: "The Minecraft Movie",
+    img: "the Minecraft Movie.jpg",
+    time: "2h 09min",
+    yer: 2025,
+    reting: 3.5,
+    coalite: "4k",
+    type: "Animation/Family"
+  }
+];
 
 
-const data_c3=[
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:2,
-    coalite:"4k",
-    type:"Action"
 
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:2,
-    coalite:"4k",
-    type:"Action"
+const data_c2 = [
+  {
+    name: "Ne Zha 2",
+    img: "…", 
+    time: "4", 
+    yer: 2025, 
+    reting: 4, 
+    coalite: "4k", 
+    type: "Animation/Fantasy"
+  },
+  {
+    name: "Lilo & Stitch",
+    img: "…", 
+    time: "4", 
+    yer: 2025, 
+    reting: 4, 
+    coalite: "4k", 
+    type: "Action/Adventure"
+  },
+  {
+    name: "A Minecraft Movie",
+    img: "…", 
+    time: "4", 
+    yer: 2025, 
+    reting: 4, 
+    coalite: "4k", 
+    type: "Animation/Family"
+  },
+  {
+    name: "Jurassic World Rebirth",
+    img: "…", 
+    time: "4", 
+    yer: 2025, 
+    reting: 4, 
+    coalite: "4k", 
+    type: "Sci‑Fi/Adventure"
+  },
+  {
+    name: "How to Train Your Dragon (2025)",
+    img: "…", 
+    time: "4", 
+    yer: 2025, 
+    reting: 4, 
+    coalite: "4k", 
+    type: "Adventure"
+  },
 
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:2,
-    coalite:"4k",
-    type:"Action"
+  {
+    name: "Superman",
+    img: "…", 
+    time: "4", 
+    yer: 2025, 
+    reting: 4, 
+    coalite: "4k", 
+    type: "Superhero/Action"
+  },
 
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:2,
-    coalite:"4k",
-    type:"Action"
+  {
+    name: "F1: The Movie",
+    img: "…", 
+    time: "4", 
+    yer: 2025, 
+    reting: 4, 
+    coalite: "4k", 
+    type: "Sports/Action"
+  },
+  {
+    name: "Captain America: Brave New World",
+    img: "…", 
+    time: "4", 
+    yer: 2025, 
+    reting: 4, 
+    coalite: "4k", 
+    type: "Superhero/Action"
+  },
+  {
+    name: "Sinners",
+    img: "…", 
+    time: "4", 
+    yer: 2025, 
+    reting: 4, 
+    coalite: "4k", 
+    type: "Horror/Thriller"
+  },
+ 
+  {
+    name: "Thunderbolts*",
+    img: "…", 
+    time: "4", 
+    yer: 2025, 
+    reting: 4, 
+    coalite: "4k", 
+    type: "Superhero/Action"
+  }
+];
 
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:2,
-    coalite:"4k",
-    type:"Action"
 
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:2,
-    coalite:"4k",
-    type:"Action"
 
-},
-{
-    name:"The Lord of the Rings",
-    img:"../images/thelord.jpg",
-    time:"2h 30min",
-    yer:2012,
-    reting:2,
-    coalite:"4k",
-    type:"Action"
+const data_c3 = [
+  {
+    name: "The Chronicles of Narnia: The Lion, the Witch and the Wardrobe",
+    img: "../images/narnia_lion_witch.jpg",
+    time: "2h 23min",
+    yer: 2005,
+    reting: 4,
+    coalite: "4k",
+    type: "Adventure"
+  },
+  {
+    name: "Willow",
+    img: "../images/willow_1988.jpg",
+    time: "2h 6min",
+    yer: 1988,
+    reting: 3,
+    coalite: "4k",
+    type: "Adventure"
+  },
+  {
+    name: "Legend",
+    img: "../images/legend_1985.jpg",
+    time: "1h 29min",
+    yer: 1985,
+    reting: 3,
+    coalite: "HD",
+    type: "Fantasy"
+  },
+  {
+    name: "Excalibur",
+    img: "../images/excalibur_1981.jpg",
+    time: "2h 21min",
+    yer: 1981,
+    reting: 3,
+    coalite: "HD",
+    type: "Medieval Fantasy"
+  },
+  {
+    name: "Stardust",
+    img: "../images/stardust_2007.jpg",
+    time: "2h 8min",
+    yer: 2007,
+    reting: 4,
+    coalite: "4k",
+    type: "Romance"
+  },
+  {
+    name: "Immortals",
+    img: "../images/immortals_2011.jpg",
+    time: "1h 50min",
+    yer: 2011,
+    reting: 3,
+    coalite: "4k",
+    type: "Epic Fantasy"
+  },
+  {
+    name: "The Green Knight",
+    img: "../images/green_knight_2021.jpg",
+    time: "2h 10min",
+    yer: 2021,
+    reting: 3,
+    coalite: "4k",
+    type: "Mythic Fantasy"
+  },
+  {
+    name: "Dungeons & Dragons: Honor Among Thieves",
+    img: "../images/dungeons_dragons_2023.jpg",
+    time: "2h 5min",
+    yer: 2023,
+    reting: 3,
+    coalite: "4k",
+    type: "Adventure"
+  },
+  {
+    name: "The Hobbit: An Unexpected Journey",
+    img: "../images/hobbit_unexpected.jpg",
+    time: "2h 49min",
+    yer: 2012,
+    reting: 4,
+    coalite: "4k",
+    type: "Adventure"
+  },
+  {
+    name: "The Hobbit: The Desolation of Smaug",
+    img: "../images/hobbit_desolation.jpg",
+    time: "2h 41min",
+    yer: 2013,
+    reting: 4,
+    coalite: "4k",
+    type: "Adventure"
+  },
+  {
+    name: "The Hobbit: The Battle of the Five Armies",
+    img: "../images/hobbit_battle.jpg",
+    time: "2h 24min",
+    yer: 2014,
+    reting: 4,
+    coalite: "4k",
+    type: "Adventure"
+  },
+  {
+    name: "The Chronicles of Narnia: Prince Caspian",
+    img: "../images/narnia_prince_caspian.jpg",
+    time: "2h 32min",
+    yer: 2008,
+    reting: 3,
+    coalite: "4k",
+    type: "Adventure"
+  }
+];
 
-},
-]
 
 let c1 = document.getElementById("c1")
 data_c1.map((movie)=>{
@@ -238,7 +336,8 @@ data_c1.map((movie)=>{
     c1.innerHTML +=`
     
      <div class="box-movie-title-contener">
-             <img src="../images/thelord.jpg" alt="">
+            <i id="sv" class="fa-solid fa-bookmark"></i>
+             <img src="../images/${movie.img}" alt="">
              <div class="info-movie">
  
                  <h1 class="name-mobie">${movie.name}</h1> <br>
@@ -256,11 +355,7 @@ data_c1.map((movie)=>{
     `
 
 })
-let view=document.getElementById("play-m")
-view.addEventListener("click",function(e){
-    console.log(e.target);
-                  
-})
+
 
 
 let c2 = document.getElementById("c2")
@@ -269,10 +364,11 @@ data_c2.map((movie)=>{
     for (let reting = 0; reting < movie.reting; reting++) {
        reting_html.push('<i class="fa-regular fa-star"></i>') 
     }
-    c2.innerHTML +=`
+   c2.innerHTML+=`
     
      <div class="box-movie-title-contener">
-             <img src="../images/thelord.jpg" alt="">
+            <i id="sv" class="fa-solid fa-bookmark"></i>
+             <img src="${movie.img}" alt="">
              <div class="info-movie">
  
                  <h1 class="name-mobie">${movie.name}</h1> <br>
@@ -300,6 +396,7 @@ data_c3.map((movie)=>{
     c3.innerHTML +=`
     
      <div class="box-movie-title-contener">
+                 <i id="sv" class="fa-solid fa-bookmark"></i>
              <img src="../images/thelord.jpg" alt="">
              <div class="info-movie">
  
@@ -319,3 +416,17 @@ data_c3.map((movie)=>{
 
 })
 
+
+
+//save favorate list
+let sv = document.getElementById("sv")
+let Favorate = document.getElementById("Favorate")
+sv.addEventListener("click",function(){   
+localStorage.setItem("vaforate","saved")
+sv.style.fontSize="40px"
+Favorate.style.color="red"
+
+})
+if(localStorage.getItem("vaforate") === "saved"){
+    Favorate.style.color="red"
+}
